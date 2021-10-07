a0cb1128fc79c2e7061ab0bc02649d14c3d78f9e
sahrayali2000 <sahayali2000@gmail.com>
2021-10-07 11:45:31 +0330

    search was made completly

--- products/api/views.py
+++ products/api/views.py
@@ -20,9 +20,12 @@ class Search(mixins.ListModelMixin,
         search_serializer = SearchSerializer(data=request.data)
         if search_serializer.is_valid():
             try:
-                search_instance = get_object_or_404(Product, name__startswith=search_serializer.validated_data['search_input'])
+                search_instance = get_list_or_404(Product, name__startswith=search_serializer.validated_data['search_input'])
+                MyData = dict()
+                for item in search_instance:
+                    MyData.update({f'{item.name}': item.id})
 
-                data = {'product': {f'{search_instance.name}': search_instance.id}}
+                data = {'product': MyData}
                 return Response(data=data, status=status.HTTP_200_OK)
             except:
                 try:
